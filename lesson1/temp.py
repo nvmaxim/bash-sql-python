875894eecbb3aba355f6738b69e0136bc867bb18
nvmaxim <m.novikov2050@yandex.ru>
2026-02-08 02:24:54 +0300

    update

--- lesson1/temp.py
+++ lesson1/temp.py
@@ -1,43 +1,59 @@
-# Front Как, не используя методы списков и строк, по числу n построить число, в котором n записано 100 раз подряд (например, из n = 15 получить 1515...15 100 раз), а затем вывести это число и его квадрат?
-
+from _001_main import df_csv
+import pandas as pd
+import numpy as np
 import sys
 import io
+from typing import Union, Optional
+
+sys.setrecursionlimit(2000)
+pd.set_option("display.max_columns", None)
+pd.set_option("display.width", 1000)
 
 
-def get_algo_logic(n: int) -> tuple[str, str]:
-    x = 0
-    len_n = 0
-    temp = n
-    while temp > 0:
-        len_n += 1
-        temp //= 10
-    power = 10**len_n
-    for _ in range(100):
-        x = x * power + n
-    return str(x), str(x * x)
+def get_pandas_logic(df: pd.DataFrame) -> pd.DataFrame:
+    result = (
+        df.groupby("manager")
+        .agg(total_revenue=("revenue", "sum"))
+        .sort_values("total_revenue", ascending=False)
+        .reset_index()
+    )
+    return result
 
 
-def solve(data_input: str) -> None:
-    if not data_input:
+def solve(data_input: Optional[pd.DataFrame]) -> None:
+    if data_input is None:
         return
-    try:
-        input_data = data_input.split()
-        n = int(input_data[0])
-        number, square = get_algo_logic(n)
-        sys.stdout.write(number + "\n")
-        sys.stdout.write(square + "\n")
-    except Exception:
-        pass
+    if isinstance(data_input, pd.DataFrame):
+        if data_input.empty:
+            return
+        result = get_pandas_logic(data_input)
+        print("--- PANDAS RESULT ---")
+        print(result.to_string(index=False, encoding="utf-8"))
+        print("-" * 30)
+        return
+    return
 
 
 def main():
     IS_LOCAL_TEST = True
-    if IS_LOCAL_TEST:
-        raw_data = 15
-        input_steam = io.StringIO(raw_data).read
-    else:
-        input_steam = sys.stdin.read()
-    solve(input_steam)
+    CURRENT_MODE = "PANDAS"  # OR ALGO
+
+    if CURRENT_MODE == "ALGO":
+        if IS_LOCAL_TEST:
+            raw_input = ""
+            input_stream = io.StringIO(raw_input).read()
+        else:
+            input_stream = sys.stdin.read()
+        # solve(input_stream)
+    elif CURRENT_MODE == "PANDAS":
+        if IS_LOCAL_TEST:
+            df = df_csv.copy()
+            table_q1 = df[df["period"] == "Q1"].reset_index(drop=True)
+            table_q2 = df[df["period"] == "Q2"].reset_index(drop=True)
+            df = pd.concat([table_q1, table_q2], ignore_index=True)
+        else:
+            df = pd.read_csv(sys.stdin)
+        solve(df)
 
 
 if __name__ == "__main__":
